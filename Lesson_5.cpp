1f2406f994e15d6570bf7908b557cfee80c56c14
all4nitrous <all4nitrous@gmail.com>
2025-01-09 18:18:53 -0500

    compiler issue solved

--- Lesson_5.cpp
+++ Lesson_5.cpp
@@ -1,1 +1,20 @@
+/* Lesson on namespaces*/
 #include <iostream>
+
+namespace first {
+        int x = 1;
+    }
+namespace second {
+        int x = 2;
+    }
+
+
+int main() {
+
+
+    std::cout << first::x << "\n";
+    // std::cout << second::x;
+
+
+    return 0;
+}
