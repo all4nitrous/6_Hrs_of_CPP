e44db8711da395201d7b24358334ca66a640fe78
all4nitrous <all4nitrous@gmail.com>
2025-01-09 22:52:27 -0500

    Lesson on arithmetic operators close to finished

--- Lesson_6.cpp
+++ Lesson_6.cpp
@@ -3,6 +3,23 @@
 
 int main() {
 
+    using namespace std;
+
+    int students = 20;
+
+    // students = students + 1;
+
+    // students += 1; // Can do multiple increments
+
+    // students++; // single increment
+
+    // students--;
+
+    // students -=2;
+
+    students *=2;
+
+    cout << students << endl;
 
     return 0;
 }
