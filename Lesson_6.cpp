5d41072aee501d38105fd3977323874e29233f8d
all4nitrous <all4nitrous@gmail.com>
2025-01-09 23:03:55 -0500

    Lesson on arithmetic operators completed

--- Lesson_6.cpp
+++ Lesson_6.cpp
@@ -7,6 +7,12 @@ int main() {
 
     int students = 20;
 
+    double learners = 20;
+
+    int remainder = students % 3;
+
+
+
     // students = students + 1;
 
     // students += 1; // Can do multiple increments
@@ -17,9 +23,15 @@ int main() {
 
     // students -=2;
 
-    students *=2;
+    // students *=2;
+
+    // students /= 2;
+
+    learners /= 3;
 
     cout << students << endl;
+    cout << learners << endl;
+    cout << remainder << endl;
 
     return 0;
 }
