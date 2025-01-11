b08400a55e0b59c8ecdd350f6e00ceb5cc9e3a3f
all4nitrous <all4nitrous@gmail.com>
2025-01-11 13:38:48 -0500

    Lesson_7 finished

--- Lesson_7.cpp
+++ Lesson_7.cpp
@@ -7,9 +7,20 @@ int main() {
 
     // int x = 3.14;
 
-    double x = 3.14;
+    double x = 3.14; //Implicit = Automatic
+
+    // double x = (int) 3.14; Explicit = Precede value with new data types
+
+    // char`x = 100;
 
     cout << x << endl;
+    cout << (char) 100 << endl;
+
+    int correct = 8;
+    int questions = 10;
+    double score = correct /(double)questions * 100;
+
+    cout << score << "%" << endl;
 
 
     return 0;
