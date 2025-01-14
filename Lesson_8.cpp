1652a83a3e24b27b5318d47a7a1310fee82583b1
all4nitrous <all4nitrous@gmail.com>
2025-01-14 12:47:24 -0500

    Lesson 8 on user input finished

--- Lesson_8.cpp
+++ Lesson_8.cpp
@@ -13,11 +13,20 @@ int main() {
     using namespace std;
 
     string name;
+    int age;
 
-    cout << "What's your name?: " << endl;
-    cin >> name;
+    cout << "What's your full name?: " << endl;;
+    getline(cin >> ws, name);
 
-    cout << "Hello " << name;
+    cout << endl;
+
+    cout << "What's your age?: " << endl;
+    cin >> age;
+
+    cout << endl;
+
+    cout << "Hello " << name << "!" << endl;
+    cout << "you are " << age << " years old!" << endl;
 
     return 0;
 }
