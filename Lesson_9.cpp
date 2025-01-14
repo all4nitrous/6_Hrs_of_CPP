9ac6d18fa1c7db841106de905fbf9e6540a453b6
all4nitrous <all4nitrous@gmail.com>
2025-01-14 13:14:03 -0500

    Lesson 9 finished

---
+++ Lesson_9.cpp
@@ -0,0 +1,26 @@
+/* Lesson on Math related functions */
+#include <iostream>
+#include <cmath>
+
+
+
+int main() {
+
+    using namespace std;
+
+    double x = 3.1;
+    double y = 4;
+    double z;
+
+    // z = max(x , y);
+    // z = min(x , y);
+    // z = pow(2 , 4);
+    // z = sqrt(9);
+    // z = abs(-3); /* Absoulute value function*/
+    // z = round(x);
+    z = ceil(x);
+
+    cout << z << endl;
+
+    return 0;
+}
