db9278cc28f2fe7c1992d7c601bd623f061c1719
all4nitrous <all4nitrous@gmail.com>
2025-01-14 15:20:01 -0500

    Lesson 9 finished

--- Lesson_9.cpp
+++ Lesson_9.cpp
@@ -16,7 +16,7 @@ int main() {
     // z = min(x , y);
     // z = pow(2 , 4);
     // z = sqrt(9);
-    // z = abs(-3); /* Absoulute value function*/
+    // z = abs(-3); /* Absoulute value function */
     // z = round(x);
     z = ceil(x);
 
