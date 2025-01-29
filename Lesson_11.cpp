d2000b96c6015f48c672d07812f12420fa83bd79
all4nitrous <all4nitrous@gmail.com>
2025-01-29 12:04:34 -0500

    Lesson 11 finished

--- Lesson_11.cpp
+++ Lesson_11.cpp
@@ -3,7 +3,24 @@
 
 int main() {
 
+    using namespace std;
 
+    int age;
+
+    cout << "Enter your age: " << endl;
+
+    cin >> age;
+
+    if(age >= 18) {
+        cout << "Welcome to the site!" << endl;
+    } 
+    else if (age < 0)
+    {
+        cout << "You haven't been born yet!" << endl;
+    }
+    else {
+        cout << "You are not old enough to enter!" << endl;
+    }
 
     return 0;
 }
