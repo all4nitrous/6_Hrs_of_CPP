e3319a6ca107e25249c5e27a4000c91f813d8361
all4nitrous <all4nitrous@gmail.com>
2025-02-01 13:42:44 -0500

    Lesson 20 finished

---
+++ Lesson_20.cpp
@@ -0,0 +1,18 @@
+/* Lesson on For Loops */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    for(int i = 5; i >= 1; i--) {
+
+        cout << i << endl;
+
+    }
+
+        cout << "Happy New Year" << endl;
+
+    return 0;
+}
