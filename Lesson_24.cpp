c5bc7a08c56591744ec3c65aa06e5e1f645d9715
all4nitrous <all4nitrous@gmail.com>
2025-02-01 21:27:40 -0500

    Lesson 24 finished

---
+++ Lesson_24.cpp
@@ -0,0 +1,31 @@
+/* Random Event Generator */
+
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int main() 
+{
+
+    srand(time(0));
+    int randNum = rand() % 5 + 1;
+
+    switch(randNum)
+    {
+        case 1: cout << "You win a bumber sticker!" << endl;
+                break;
+        case 2: cout << "You win a t-shirt!" << endl;
+                break;
+        case 3: cout << "You win a free lunch!" << endl;
+                break;
+        case 4: cout << "You win a gift card!" << endl;
+                break;
+        case 5: cout << "You win a concert ticket!" << endl;
+                break;
+    }
+
+    return 0;
+
+
+}
