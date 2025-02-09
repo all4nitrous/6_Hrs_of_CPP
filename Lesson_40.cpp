03345b3b8995ec072a43238e599790ee6a2e7e3c
all4nitrous <all4nitrous@gmail.com>
2025-02-08 23:39:35 -0500

    Lesson 40 continued

---
+++ Lesson_40.cpp
@@ -0,0 +1,17 @@
+/* Multidimensional arrays */
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+using std::cin;
+
+int main() 
+{
+
+    std::string cars[][3] = {{"Mustang", "Escape", "F-150"},
+                             {"Corvette", "Equinox", "Silverado"},
+                             {"Challenger", "Durango", "Ram 1500"}};
+    cout <<cars{0}
+    return 0;
+}
