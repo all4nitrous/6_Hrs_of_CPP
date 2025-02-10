b70406d838310c04d3f5300bc4f8a07092ea5c98
all4nitrous <all4nitrous@gmail.com>
2025-02-10 10:38:26 -0500

    Lesson 41 on Memory addresses finished

--- Lesson_41.cpp
+++ Lesson_41.cpp
@@ -1,10 +1,20 @@
 /* Memory Addresses */
 #include <iostream>
 
+using std::cout;
+using std::cin;
+using std::endl;
+
 int main() 
 {
 
-    
+    std::string name = "Nicholas";
+    int age = 28;
+    bool student = true;
+
+    cout << &name << endl;
+    cout << &age << endl;
+    cout << &student << endl;
 
     return 0;
 }
